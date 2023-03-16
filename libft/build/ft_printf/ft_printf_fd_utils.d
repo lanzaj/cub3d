89cc7a2a1d53efccd006d2f7816dd20b36b00a56
Maxence BOCQUEL <maxence.bocquel@gmail.com>
2023-03-16 16:49:04 +0100

    clean

--- libft/build/ft_printf/ft_printf_fd_utils.d
+++
@@ -1,6 +0,0 @@
-build/ft_printf/ft_printf_fd_utils.o: src/ft_printf/ft_printf_fd_utils.c \
-  src/ft_printf/ft_printf.h src/ft_printf/../libft.h
-
-src/ft_printf/ft_printf.h:
-
-src/ft_printf/../libft.h:
