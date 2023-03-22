69b313479f127b39bb6326d8189f7b0b1696d326
Joseph LANZA <jlanza@student.42.fr>
2023-03-22 18:10:20 +0100

    proto parsing WIP

--- libft/build/ft_lst/ft_lstiter.d
+++
@@ -1,3 +0,0 @@
-build/ft_lst/ft_lstiter.o: src/ft_lst/ft_lstiter.c src/ft_lst/../libft.h
-
-src/ft_lst/../libft.h:
--- libft/build/ft_printf/ft_printf.d
+++
@@ -1,6 +0,0 @@
-build/ft_printf/ft_printf.o: src/ft_printf/ft_printf.c \
-  src/ft_printf/ft_printf.h src/ft_printf/../libft.h
-
-src/ft_printf/ft_printf.h:
-
-src/ft_printf/../libft.h:
