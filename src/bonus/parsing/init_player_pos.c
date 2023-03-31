79a9240ac8f2235f5a019675aaa897e993959567
Maxence BOCQUEL <maxence.bocquel@gmail.com>
2023-03-31 17:56:53 +0200

    commit version avec porte et sans segfault pour les map sans start pos

--- src/bonus/parsing/init_player_pos.c
+++ src/bonus/parsing/init_player_pos.c
@@ -6,13 +6,13 @@
 /*   By: mbocquel <mbocquel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/26 22:23:21 by jlanza            #+#    #+#             */
-/*   Updated: 2023/03/29 13:32:00 by mbocquel         ###   ########.fr       */
+/*   Updated: 2023/03/31 17:54:42 by mbocquel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
 
-static void	get_starting_pos(t_param *prm, int *x, int *y)
+static int	get_starting_pos(t_param *prm, int *x, int *y)
 {
 	*y = 0;
 	while (prm->map.map[*y])
@@ -24,11 +24,12 @@ static void	get_starting_pos(t_param *prm, int *x, int *y)
 				|| prm->map.map[*y][*x] == 'S'
 				|| prm->map.map[*y][*x] == 'E'
 				|| prm->map.map[*y][*x] == 'W')
-				return ;
+				return (0);
 			(*x)++;
 		}
 		(*y)++;
 	}
+	return (1);
 }
 
 static void	init_east_west_player_pos(t_param *prm, int x, int y)
@@ -76,7 +77,8 @@ void	init_player_pos(t_param *prm)
 	int	x;
 	int	y;
 
-	get_starting_pos(prm, &x, &y);
+	if (get_starting_pos(prm, &x, &y))
+		check_map_error(prm, "Error: No starting position\n");
 	prm->pos_player.x = (double)x + 0.5;
 	prm->pos_player.y = (double)y + 0.5;
 	init_east_west_player_pos(prm, x, y);
